bb2c23d3a418202933b1bdf75acaa24d01dab4da
Chalber <chaim@cybersync.net>
2021-06-14 18:43:44 -0400

    added min_last_updated_datetime field
    https://plaid.com/docs/api/products/#accounts-balance-get-request-min-last-updated-datetime_options

--- src/Plaid/Options/AccountOptions.cs
+++ src/Plaid/Options/AccountOptions.cs
@@ -16,5 +16,13 @@ namespace Going.Plaid.Options
 		/// <remarks>An error will be returned if a provided <c>account_id</c> is not associated with the <see cref="Entity.Item"/>.</remarks>
 		[JsonPropertyName("account_ids")]
 		public string[] AccountIds { get; set; } = null!;
+		
+		/// <summary>
+		/// Indicating the oldest acceptable balance when making a request to /accounts/balance/get.
+		/// </summary>
+		/// <remarks>This field is only used when the institution is ins_128026 (Capital One), in which case a value must be provided or an INVALID_REQUEST error will be returned. For all other institutions, this field is ignored.</remarks>
+		[JsonPropertyName("min_last_updated_datetime")]
+		public DateTime? MinLastUpdatedDatetime { get; set;} = null
+		
 	}
 }
